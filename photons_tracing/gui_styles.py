4991ac60fd7a2db013c9af4b918f0e7741ea9161
Aurora Sirigu <aurora.sirigu@5bits.it>
2023-12-05 11:06:08 +0100

    feat: implement check validation messages logic and UI; initialize error handling

--- photons_tracing/gui_styles.py
+++ photons_tracing/gui_styles.py
@@ -163,7 +163,33 @@ class GUIStyles:
             color: #f8f8f8;
             selection-background-color: #8d4ef2;
             }   
-        """    
+        """   
+
+    @staticmethod     
+    def set_msg_box_style():
+        return """
+            QMessageBox {
+                background-color: #080808;   
+            }
+            QMessageBox QLabel {
+                color: #f8f8f8;
+                font-family: "Montserrat";
+                font-weight: 300;
+                font-size: 16px;
+            }
+            QMessageBox QIcon {
+                width: 20px;
+            }  
+            QMessageBox QPushButton {
+                background-color: #181818;
+                color: white;
+                width: 150px;
+                padding: 12px;
+                font-size: 14px;
+                font-family: "Montserrat";
+            }   
+                 
+        """ 
         
 
 
