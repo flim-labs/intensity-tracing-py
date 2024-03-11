5c975fa877187018c7b4c29aa2183d2125851b5c
Aurora Sirigu <aurora.sirigu@5bits.it>
2024-03-11 18:14:22 +0100

    fix: fix console.py returning only 0 values (photons count)

--- console.py
+++ console.py
@@ -1,17 +1,14 @@
 import threading
-
 import flim_labs
 
 
 def stop():
     print("stop")
 
-
-def process(time, _x, counts):
+def process(time):
     seconds = round(time / 1_000_000_000, 5)
     seconds = str(seconds).zfill(5)
-    print("[" + seconds + "s] " + str(counts[0]))
-
+    return "[" + seconds + "s]"
 
 def thread_function():
     print("Thread: Start reading from queue")
@@ -26,12 +23,14 @@ def thread_function():
                     continue_reading = False
                     break
                 ((time,), (ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8)) = v
-                print("Time=" + str(time) + " Channel 1=" + str(ch1))
+                channels = [f"Ch{i}={v}" for i, v in enumerate((ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8), start=1) if v != 0]
+                if channels:
+                    print(process(time) + " " + " ".join(channels))
 
 
 if __name__ == "__main__":
     result = flim_labs.start_intensity_tracing(
-        enabled_channels=[0],
+        enabled_channels=[1],
         bin_width_micros=1000,
         acquisition_time_millis=3000,
         write_bin=False,
@@ -40,7 +39,7 @@ if __name__ == "__main__":
     x = threading.Thread(target=thread_function)
     x.start()
     x.join()
-    # print result
+  
     bin_file = result.bin_file
     data_file = result.data_file
     print("Binary file=" + str(bin_file))
